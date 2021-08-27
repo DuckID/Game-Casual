3a74095bff426aa34e81e58389487f03aeb4cf6a
Muhammad Multazam <88530536+DuckID@users.noreply.github.com>
2021-08-27 14:52:51 +0700

    Materi 4

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour {
+
+	public string EnterScene;
+	public string EscapeScene;
+	public bool isEscapeForQuit = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Return)) {
+			Debug.Log ("Name Scene: " + EnterScene);
+			UnityEngine.SceneManagement.SceneManager.LoadScene(EnterScene);
+		}
+		if (Input.GetKeyUp (KeyCode.Escape)) {
+			if (isEscapeForQuit) {
+				Application.Quit ();
+			} else {
+				Debug.Log ("Name Scene: " + EscapeScene);
+				UnityEngine.SceneManagement.SceneManager.LoadScene(EnterScene);
+			}
+		}
+	}
+}
